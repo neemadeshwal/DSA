ece28b84ac0b2cc917018944c970ef9eb2feb610
Neema deshwal <neemadeshwal.py@gmail.com>
2024-12-10 14:06:23 +0530

    reverseAscii done

---
+++ Strings/ReverseAscii.java
@@ -0,0 +1,40 @@
+public class ReverseAscii {
+    public static void main(String[] args) {
+        
+        System.out.println("-------reverse asciii -----------");
+        System.out.println();
+
+        // String str="aBcDe"; //bAdCf
+       String str="bb"; //ca
+
+       //
+
+       char str2='a';
+
+       char nextchar=(char)(str2-1);
+
+       System.out.println(nextchar);
+
+       System.out.println(StrReverseAscii(str));
+
+    }
+
+    public static String StrReverseAscii(String str){
+
+        StringBuilder rev=new StringBuilder();
+        for(int i=0;i<str.length();i++ ){
+            char chr=str.charAt(i);
+            
+            if(i%2==0){
+                ///type casting  as chr+1 will give a integer by default and typecast back to char to get ascii val .     
+                rev.append((char)(chr+1));
+            }
+            else{
+                rev.append((char)(chr-1));
+
+            }
+        }
+
+        return rev.toString();
+    }
+}
